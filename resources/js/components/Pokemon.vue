2a829b6f1c4cd340611e42ae4aa7669e31caeea1
Max <m.bongers@student.avans.nl>
2022-11-09 20:56:20 +0100

    included tailwind

---
+++ resources/js/components/Pokemon.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1 class="text-5xl font-medium mb-5">{{this.name}}</h1>
+</template>
+
+<script>
+export default {
+    name: "Pokemon",
+
+    props: {
+        name: String
+    },
+
+    data() {
+        return{
+            properties: []
+        }
+    },
+
+    mounted() {
+        axios.get("https://pokeapi.co/api/v2/pokemon/" + this.name)
+            .then(r => this.properties = r.data)
+            .catch(err => console.log(err));
+    }
+}
+</script>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/pokemon/{name}', function ($name){
+    return view('pokemon', ['name' => $name]);
 });
